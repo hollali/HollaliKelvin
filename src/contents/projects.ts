cf4a3db77dffb44acb72e0bc746b95b4ab10c2a4
hollali <dheztinykartel@gmail.com>
2025-09-30 16:06:44 +0000

    `Update projects.ts with new demo link for Stock Market app project`

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     description: 'Stock Market app built with Next.js, Shadcn, Better Auth, and Inngest. Track prices, set alerts, explore insights, manage watchlists, and automate workflows for notifications and analytics',
     technologies: ['NextJs', 'Tailwind css', 'React', 'Node.js', 'MongoDB', 'Socket.io', ' Inngest', 'Shadcn UI', 'Better Auth'],
     githubLink: 'https://github.com/tradeflow',
-    demoLink: 'https://demo.com',
+    demoLink: 'https://tradeflowpulse.netlify.app/',
     image: '/projects/stock-market.png',
   },
   {
