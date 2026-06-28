e73149bbb4108132809b97370e98e0a1dfc16fb6
hollali <dheztinykartel@gmail.com>
2026-06-28 11:37:01 +0000

    all

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,3 +1,3 @@
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dp2x4nnb'
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
